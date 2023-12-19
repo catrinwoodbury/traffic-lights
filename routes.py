89e708c31da5255add7f9f2cb3ed6a040f200e39
catrinwoodbury <woodbury.catrin@gmail.com>
2023-12-19 11:09:58 -0700

    routes

---
+++ routes.py
@@ -0,0 +1,11 @@
+import json
+#with open('data.json') as access_json:
+ #   read_content = json.load(access_json)
+#lights = (read_content['lights'])
+#print((lights[1]['latitude']))
+
+with open("intervals.json") as interval_data:
+    data = json.load(interval_data)
+    gra = "north"
+green_time = (data["intersections"][0]["direction"][gra]["green_time"])
+print(green_time)
