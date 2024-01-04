47ca815b1c13e132d193adb65c250edf4ba7528e
catrinwoodbury <woodbury.catrin@gmail.com>
2024-01-04 15:16:49 -0700

    messing around with directions api

--- api_requests.py
+++ api_requests.py
@@ -1,33 +1,39 @@
-import requests
+import datetime
+from datetime import timedelta
+import math
 import json
+import requests
 from googlemaps import convert
 
 with open("api_key.json") as api:
     authent = json.load(api)
 api_key = str(authent["api_key"])
 
-with open ("intervals.json") as info:
-    data = json.load(info)
 
-url = "https://maps.googleapis.com/maps/api/distancematrix/json"
+with open("intervals.json") as interval_data:
+    data = json.load(interval_data) 
 
-olat = (data["intersections"][2]["lat"])
-olng = (data["intersections"][2]["lng"])
+starting_point = input("Enter the address of the starting location:\n")
+end_point = input("Enter the address of your destination:\n")
 
+arrival_time = input("Input your desired arrival time in YYYY-MM-DD-HH-MM-SS format: ")
+year, month, day, hour, minute, second = map(int, arrival_time.split('-'))
+time_arrival = datetime.datetime(year, month, day, hour, minute, second)
+
+olat = (data["intersections"][0]["lat"])
+olng = (data["intersections"][0]["lng"])
 dlat = (data["intersections"][1]["lat"])
 dlng = (data["intersections"][1]["lng"])
 
-origin = {"lat": olat, "lng": olng}
-destination = {"lat":dlat, "lng": dlng}
+waypoints = [{"lat": olat, "lng": olng}, {"lat": dlat, "lng": dlng}]
 
-parameters = {"origins": convert.location_list(origin), "destinations": convert.location_list(destination), "departure_time": 1704115851, "key": api_key}
+url_directions = "https://maps.googleapis.com/maps/api/directions/json"
 
-response = requests.get(url, params=parameters)
 
-jsonapi =(response.json())
 
-final = json.dumps(jsonapi, indent = 4)
-
-duration = (jsonapi["rows"][0]["elements"][0]["duration"]["text"])
-
-print(duration)
+###DIRECTIONS
+parameters_directions = {"origin": starting_point, "destination": end_point,  "arrival_time": convert.time(time_arrival), "waypoints": convert.location_list(waypoints), "key": api_key}
+response_directions = requests.get(url_directions, params=parameters_directions)
+json_directions = (response_directions.json())
+final_directions = json.dumps(json_directions, indent = 4)
+print(final_directions)
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import datetime
-import time
 from datetime import timedelta
 import math
 import json
@@ -13,8 +12,16 @@ api_key = str(authent["api_key"])
 with open("intervals.json") as interval_data:
     data = json.load(interval_data)
 
+starting_point = input("Enter the address of the starting location:\n")
+end_point = input("Enter the address of your destination:\n")
 
-url = "https://maps.googleapis.com/maps/api/distancematrix/json"
+arrival_time = input("Input your desired arrival time in YYYY-MM-DD-HH-MM-SS format: ")
+year, month, day, hour, minute, second = map(int, arrival_time.split('-'))
+time_arrival = datetime.datetime(year, month, day, hour, minute, second)
+
+
+url_distance = "https://maps.googleapis.com/maps/api/distancematrix/json"
+url_directions = "https://maps.googleapis.com/maps/api/directions/json"
 
 olat = (data["intersections"][0]["lat"])
 olng = (data["intersections"][0]["lng"])
@@ -25,14 +32,19 @@ dlng = (data["intersections"][1]["lng"])
 origin = {"lat": olat, "lng": olng}
 destination = {"lat":dlat, "lng": dlng}
 
+###DIRECTIONS
+parameters_directions = {"origins": starting_point, "destinations": end_point,  "arrival_time": convert.time(time_arrival), "key": api_key}
+response_directions = requests.get(url_directions, params=parameters_directions)
+json_directions = (response_directions.json())
+final_directions = json.dumps(json_directions, indent = 4)
+print(final_directions)
+
+
+###DISTANCE MATRIX
 parameters = {"origins": convert.location_list(origin), "destinations": convert.location_list(destination), "departure_time": 1704115851, "key": api_key}
-
-response = requests.get(url, params=parameters)
-
+response = requests.get(url_distance, params=parameters)
 jsonapi =(response.json())
-
 final = json.dumps(jsonapi, indent = 4)
-
 light_to_light = (jsonapi["rows"][0]["elements"][0]["duration"]["value"])
 
 
@@ -48,11 +60,7 @@ green_time = datetime.datetime(year, month, day, hour, minute, second)
 ## user input desired arrival time
 ## convert time into datetime
 
-arrival_time = input("Input your desired arrival time in YYYY-MM-DD-HH-MM-SS format: ")
-year, month, day, hour, minute, second = map(int, arrival_time.split('-'))
-turn_time = datetime.datetime(year, month, day, hour, minute, second)
-
-light_time = (turn_time) - datetime.timedelta(seconds = light_to_light)
+light_time = (time_arrival) - datetime.timedelta(seconds = light_to_light)
 
 ## subtract the arrival time from the turn time
 between_time = (light_time) - (green_time)
