4d3c9cfe8f1e0bbbf47fffa29cfa94368a047f24
catrinwoodbury <woodbury.catrin@gmail.com>
2024-01-09 09:34:12 -0700

    testing

--- cardinal.py
+++ cardinal.py
@@ -1,16 +1,35 @@
 import math
 
-destination_x =   39.6044273 
-origin_x = 39.575384 
-destination_y =  -105.0725764  
-origin_y = -105.0620071 
+destination_x =   39.6047322 
+origin_x = 39.6044273 
+destination_y = -105.0737276
+origin_y = -105.0725764
 
     
 cardinal_degree = (math.degrees(math.atan2(((destination_y)-(origin_y)),((destination_x)-(origin_x)))))
 
 
 if cardinal_degree < 0:
-    new_degree = cardinal_degree + 360
-    print(new_degree)
+    cardinal_degree = cardinal_degree + 360
+    print(cardinal_degree)
 else:
     print(cardinal_degree)
+
+if 337.5 <= cardinal_degree <= 360:
+    print("North")
+if 0 <= cardinal_degree < 22.5:
+    print("North")
+if 22.5 <= cardinal_degree < 67.5:
+    print("North-East")
+if 67.5 <= cardinal_degree < 112.5:
+    print("East")
+if 112.5 <= cardinal_degree < 157.5:
+    print("South-East")
+if 157.5 <= cardinal_degree < 202.5:
+    print("South")
+if 202.5 <= cardinal_degree < 247.5:
+    print("South-West")
+if 247.5 <= cardinal_degree < 292.5:
+    print("West")
+if 292.5 <= cardinal_degree < 337.5:
+    print("North-West")
