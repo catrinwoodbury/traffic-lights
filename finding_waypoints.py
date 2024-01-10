0e75010d2fcce1282a1215a0c8b280d89b57224e
catrinwoodbury <woodbury.catrin@gmail.com>
2024-01-09 22:02:08 -0700

    notes

--- finding_waypoints.py
+++ finding_waypoints.py
@@ -5,5 +5,8 @@ with open ("intervals.json") as info:
     
 lats_lngs = [latitudes["lat,lng"] for latitudes in data["intersections"]]
 
-
+for i in lats_lngs:
+    ## calculate distance from polyline (polyline for distance between origin and destination) 
+    ## if the distance is less than (a value) add to waypoints array
+    
 print(lats_lngs)
