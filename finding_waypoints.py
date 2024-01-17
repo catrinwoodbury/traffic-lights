d263bf796d289a4a49c2239a4fb4140b6e92c88a
catrinwoodbury <woodbury.catrin@gmail.com>
2024-01-16 17:59:26 -0700

    kind of working, lat longs are right but optimize waypoints is not working correctly

--- finding_waypoints.py
+++ finding_waypoints.py
@@ -8,27 +8,30 @@ wp = []
 place_waypoints = []
 R = 6373.0
 
+## opens and grabs the api key
 with open("api_key.json") as api:
     authent = json.load(api)
 api_key = str(authent["api_key"])
 
+## opens and parses the json intersection data
 f = open('intervals.json', "r")
-# Reading from file
 data = json.loads(f.read())
  
-starting_point = "7179 S Cody Way, Littleton, CO 80128"
+## start and end locations
+starting_point = "8420 W Ken Caryl Ave, Littleton, CO 80128"
 end_point = "5375 S Wadsworth Blvd, Lakewood, CO 80123"
 
+## desired arrival time
 arrival_time = input("Input your desired arrival time in YYYY-MM-DD-HH-MM-SS format: ")
 year, month, day, hour, minute, second = map(int, arrival_time.split('-'))
 time_arrival = datetime.datetime(year, month, day, hour, minute, second)
 
-
+## base api urls
 url_distance = "https://maps.googleapis.com/maps/api/distancematrix/json"
 url_directions = "https://maps.googleapis.com/maps/api/directions/json"
-url_roads = "https://roads.googleapis.com/v1/snapToRoads"
 
-###DIRECTIONS
+## DIRECTIONS calculation:
+## parameters
 parameters_directions = {"origin": starting_point,
                         "destination": end_point,  
                         "arrival_time": convert.time(time_arrival), 
@@ -36,22 +39,28 @@ parameters_directions = {"origin": starting_point,
 response_directions = requests.get(url_directions, params=parameters_directions)
 json_directions = (response_directions.json())
 final_directions = json.dumps(json_directions, indent = 4)
-#print(final_directions)
+print(final_directions)
 
-
+## gets the polyline and decodes it from the final directions
 poly_line = (json_directions["routes"][0]["overview_polyline"]["points"])
 decode = convert.decode_polyline(poly_line)
-## through the json list
+
+## locates all lat long values in intersection data
 coords = [cord["lat,lng"] for cord in data["intersections"]] 
+## for each lat long value
 for i in coords:
     file = tuple(i)
     result = convert.normalize_lat_lng(file)
+    ## breaks tuple into specific lat long coords
     lat1 = radians(result[0])
     long1 = radians(result[1])
+
+    ## for each lat long coord in the polyline
     for s in decode:
         end = convert.normalize_lat_lng(s)
         lat2 = radians(end[0])
         long2 = radians(end[1])
+        ## calculates the distance between the intersection and polyline
         dlon = long2 - long1
         dlat = lat2 - lat1
         a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
@@ -59,26 +68,18 @@ for i in coords:
         distance = R * c
         ## convert to feet
         final = distance * 3280.84
-        if final <= 400:
+        ## if the distance is less than or equal to 500 feet
+        ## add the lat long coords to the waypoints list
+        if final <= 500:
             wp.append(result)
             waypoints = list(set(wp))
-for v in waypoints:
-    parameters_roads = {"path": convert.location_list(v),
-                        "interpolate": True,
-                        "key": api_key}
-    response_roads = requests.get(url_roads, params=parameters_roads)
-    json_roads = (response_roads.json())
-    final_roads = json.dumps(json_roads, indent = 4)
-    roadId = json.loads(final_roads)
-    Ids = [id["placeId"] for id in roadId["snappedPoints"]]
-    placeid = "placeid:", Ids
-    place_waypoints.append(placeid)
-print(final_roads)
+print(waypoints)    
 
+## calculate directions between the origin and destination through the way points
 parameters_directions = {"origin": starting_point,
                         "destination": end_point,  
                         "arrival_time": convert.time(time_arrival),
-                        "waypoints": "optimize:true|" + str(place_waypoints), 
+                        "waypoints": "optimize:true|" + convert.location_list(waypoints),
                         "key": api_key}
 response_directions = requests.get(url_directions, params=parameters_directions)
 json_directions = (response_directions.json())
