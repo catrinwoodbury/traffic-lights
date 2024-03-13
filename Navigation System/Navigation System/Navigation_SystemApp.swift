af8d2eefbe172c226f4128c15c7d4a5a68adba30
Woodbury Catrin <2109176@735-STEM-14.jeffco.schools>
2024-03-13 15:14:43 -0600

    swift stuff

---
+++ Navigation System/Navigation System/Navigation_SystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_SystemApp.swift
+//  Navigation System
+//
+//  Created by Woodbury Catrin on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_SystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
